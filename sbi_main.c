40593fab611e9729f4b8a0efabbd518009f356fe
NikolaIIIII <73211868+NikolaIIIII@users.noreply.github.com>
2024-04-25 03:14:57 -1200

    Update sbi_main.c

--- sbi_main.c
+++ sbi_main.c
@@ -91,40 +91,22 @@ static int check_h_extension(void)
 void sbi_main(void)
 {
 	unsigned long val;
-
 	uart_init();
-
 	init_printk_done(putchar);
 	printk(BANNER);
-
 	sbi_trap_init();
-
-	/*
-	 * 配置PMP
-	 * 所有地址空间都可以访问
-	 */
 	sbi_set_pmp(0, 0, -1UL, PMP_RWX);
 	sbi_set_pmp(1, 0x80000000, 0x40000, PMP_RWX);
-
-	printk("H extension %s\n", check_h_extension() ? "implemented" : "not implemented");
-
-	/* 设置跳转模式为S模式 */
 	val = read_csr(mstatus);
 	val = INSERT_FIELD(val, MSTATUS_MPP, PRV_S);
 	val = INSERT_FIELD(val, MSTATUS_MPIE, 0);
 	write_csr(mstatus, val);
-
 	delegate_traps();
-
-	/* 设置M模式的Exception Program Counter，用于mret跳转 */
 	write_csr(mepc, FW_JUMP_ADDR);
-	/* 设置S模式异常向量表入口*/
 	write_csr(stvec, FW_JUMP_ADDR);
 	/* 关闭S模式的中断*/
 	write_csr(sie, 0);
-	/* 关闭S模式的页表转换 */
 	write_csr(satp, 0);
-
 	/* 切换到S模式 */
 	asm volatile("mret");
 }
