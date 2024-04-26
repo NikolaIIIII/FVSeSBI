bd6bb8522f7ff62f1fa640dca090799d40cbdeee
NikolaIIIII <73211868+NikolaIIIII@users.noreply.github.com>
2024-04-25 21:42:16 -1200

    Update sbi_main.c

--- sbi_main.c
+++ sbi_main.c
@@ -29,51 +29,29 @@ int sbi_set_pmp(int reg_idx, unsigned long start, unsigned long size, unsigned l
 	order = log2roundup(size);
 	if (order < PMP_SHIFT)
 		return -1;
-
+	
 	printk("%s: start: 0x%lx order %d prot 0x%lx\n", __func__, start, order, prot);
-
 	pmpaddr = start >> PMP_SHIFT;
-
-	/* 对于RV64，对应的cfg寄存器是pmpcfg0，pmpcfg2，pmpcfg4... */
 	pmpcfg_csr   = (CSR_PMPCFG0 + (reg_idx >> 2)) & ~1;
 	pmpcfg_shift = (reg_idx & 7) << 3;
-
 	pmpaddr_csr = CSR_PMPADDR0 + reg_idx;
-
-	/* 配置cfg中的A字段，NA4表示只有4bytes的区域 */
 	prot &= ~PMP_A;
 	prot |= (order == PMP_SHIFT) ? PMP_A_NA4 : PMP_A_NAPOT;
-
-	/* 配置cfg中的prot */
 	cfgmask = ~(0xffUL << pmpcfg_shift);
 	pmpcfg	= (read_csr_num(pmpcfg_csr) & cfgmask);
 	pmpcfg |= ((prot << pmpcfg_shift) & ~cfgmask);
-
-	/* 
-	 * 配置PMP address
-	 * 当oder == 2时，A使用PMP_A_NA4, pmpaddr直接使用start>>2
-	 * 当oder > 2时，A使用PMP_A_NAPOT，需要重新配置pmpaddr
-	 */
 	if (order > PMP_SHIFT)
 	{
 		if (order == RISCV_XLEN) {
 			pmpaddr = -1UL;
 		} else {
-			/*
-			 * 若pmpaddr值为y...y01...1，设连续1的个数为n,
-			 * 则该PMP entry所控制的地址空间为从y...y00...0开始的2^{n+3}个字节
-			 * 参考RSIC-V手册
-			 */ 
 			addrmask = (1UL << (order - PMP_SHIFT)) - 1;
 			pmpaddr	 &= ~addrmask;
 			pmpaddr |= (addrmask >> 1);
 		}
 	}
-
 	printk("%s: pmpaddr: 0x%lx  pmpcfg 0x%lx, cfs_csr 0x%x addr_csr 0x%x\n",
 			__func__, pmpaddr, pmpcfg, pmpcfg_csr, pmpaddr_csr);
-
-	/* 写CSR寄存器 */
 	write_csr_num(pmpaddr_csr, pmpaddr);
 	write_csr_num(pmpcfg_csr, pmpcfg);
 
@@ -85,9 +63,6 @@ static int check_h_extension(void)
 	return read_csr(misa) & (1 << 7);
 }
 
-/*
- * 运行在M模式
- */
 void sbi_main(void)
 {
 	unsigned long val;
@@ -104,9 +79,7 @@ void sbi_main(void)
 	delegate_traps();
 	write_csr(mepc, FW_JUMP_ADDR);
 	write_csr(stvec, FW_JUMP_ADDR);
-	/* 关闭S模式的中断*/
 	write_csr(sie, 0);
 	write_csr(satp, 0);
-	/* 切换到S模式 */
 	asm volatile("mret");
 }
